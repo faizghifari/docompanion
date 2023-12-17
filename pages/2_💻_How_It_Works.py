bedff44851085a55ecb7107f380c2a5449b7cac8
Faiz Ghifari Haznitrama <fgh1997@live.com>
2023-12-17 20:41:07 +0900

    init page title and icon

--- pages/2_💻_How_It_Works.py
+++ pages/2_💻_How_It_Works.py
@@ -1,1 +1,3 @@
 import streamlit as st
+
+st.set_page_config(page_title="How It Works - Docompanion", page_icon=":computer:")
--- pages/3_📄_Changelog.py
+++ pages/3_📄_Changelog.py
@@ -1,1 +1,3 @@
 import streamlit as st
+
+st.set_page_config(page_title="Changelog - Docompanion", page_icon=":page_facing_up:")
