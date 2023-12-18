382873ac0d828bf1c03d6c9298234731483dba64
Faiz Ghifari Haznitrama <fgh1997@live.com>
2023-12-18 16:33:32 +0900

    add function to choose wait and intro txt

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import os
 import re
 
+import random
 import requests
 
 import streamlit as st
@@ -34,6 +35,64 @@ def get_supported_files():
         "ipynb",
         "md",
     ]
+
+
+def get_random_wait_txt():
+    return random.choice(
+        [
+            "Give me some time to think and answer your question okay? Please wait...",
+            "Allow me a moment to ponder and respond to your question, alright? Your patience is appreciated.",
+            "Kindly grant me a moment to contemplate and provide an answer to your query, please. Thank you for your patience.",
+            "I need a moment to consider and address your question, if that's okay. Thank you for your understanding.",
+            "Let me take a moment to reflect and offer a response to your question, alright? Your patience is valued.",
+            "Please give me a moment to mull over and reply to your question, okay? Thank you for your patience.",
+            "I require some time to think through and respond to your question, if that's alright. Thank you for your patience.",
+            "Just need a moment to deliberate and answer your question, okay? Thank you for your understanding.",
+            "I'd appreciate a moment to consider and address your question, please. Your patience is valued.",
+            "Please grant me a moment to ponder and reply to your question, alright? Thank you for your patience.",
+            "Kindly allow me a moment to think and respond to your question, okay? Your patience is appreciated.",
+        ]
+    )
+
+
+def get_random_answer_txt(answer_len):
+    return random.choice(
+        [
+            f"""Sorry for taking too long!
+I generated {answer_len} subquestions derived from your question for more accurate answer!
+Here are my answer from the generated subquestions""",
+            f"""Apologies for the delay!
+I've created {answer_len} subquestions based on your query to enhance the accuracy of my response.
+Here are the answers derived from these subquestions.""",
+            f"""I regret the delay!
+I've produced {answer_len} subqueries stemming from your initial question to ensure a more precise response.
+Here are the resulting answers.""",
+            f"""Forgive the delay!
+I've formulated {answer_len} subqueries from your original question for increased accuracy.
+Here are the responses derived from these queries.""",
+            f"""I apologize for the wait!
+I've constructed {answer_len} subqueries from your question to provide a more precise response.
+Here are the answers resulting from these subqueries.""",
+            f"""Regretfully, it took longer than expected!
+I've generated {answer_len} subquestions from your query to ensure a more accurate response.
+Here are the answers derived from these subqueries.""",
+            f"""I'm sorry for the delay!
+I've devised {answer_len} subqueries based on your question to enhance the accuracy of my response.
+Here are the resulting answers.""",
+            f"""Apologies for the delay in response!
+I've generated {answer_len} subquestions stemming from your query for a more accurate answer.
+Here are the answers derived from these subquestions.""",
+            f"""Sorry for the delay!
+I've created {answer_len} subqueries derived from your question to provide a more accurate response.
+Here are the answers resulting from these subqueries.""",
+            f"""I regret the delay!
+I've formulated {answer_len} subquestions from your initial query to ensure a more precise response.
+Here are the resulting answers.""",
+            f"""Forgive the delay!
+I've compiled {answer_len} subqueries based on your original question for increased accuracy.
+Here are the responses derived from these queries.""",
+        ]
+    )
 
 
 def download(url, dest_folder):
