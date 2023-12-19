72c8335fac06cabd74931c75d5d9f9308c34e5d3
Faiz Ghifari Haznitrama <fgh1997@live.com>
2023-12-19 19:45:38 +0900

    update chat page

--- pages/1_💬_Chat_Companion.py
+++ pages/1_💬_Chat_Companion.py
@@ -1,9 +1,33 @@
+import os
+import sys
 import time
 import streamlit as st
 
 from streamlit_extras.switch_page_button import switch_page
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+from utils import get_random_wait_txt, get_random_answer_txt
+
 st.set_page_config(page_title="Chat - Docompanion", page_icon=":speech_balloon:")
+
+
+def write_assistant_msg(msg):
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+
+        message_placeholder.markdown("▌")
+        time.sleep(1)
+
+        for chunk in msg.split():
+            full_response += chunk + " "
+            time.sleep(0.05)
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
+
 
 if (
     "service_context" not in st.session_state
@@ -17,37 +41,34 @@ if (
 
 st.title("Chat with Your Docompanion")
 
-# Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Accept user input
 if prompt := st.chat_input("Ask your docompanion anything about your docs!"):
-    # Add user message to chat history
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
 
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        full_response = ""
-        assistant_response = st.session_state.subq_qe_engine.query(prompt)
+    wait_txt = get_random_wait_txt()
+    write_assistant_msg(wait_txt)
 
-        # Simulate stream of response with milliseconds delay
-        for chunk in assistant_response.split():
-            full_response += chunk + " "
-            time.sleep(0.05)
-            # Add a blinking cursor to simulate typing
-            message_placeholder.markdown(full_response + "▌")
-        message_placeholder.markdown(full_response)
+    assistant_response = st.session_state.subq_qe_engine.query(prompt)
+    sub_responses = [
+        node.text.replace("Response", "Answer")
+        for node in assistant_response.source_nodes
+        if node.metadata == {}
+    ]
+    final_response = "Final Answer:\n\n" + assistant_response.response
+    intro_reply = get_random_answer_txt(len(sub_responses))
 
-    # Add assistant response to chat history
-    st.session_state.messages.append({"role": "assistant", "content": full_response})
+    all_responses = [intro_reply]
+    all_responses.extend(sub_responses)
+    all_responses.append(final_response)
+
+    for resp in all_responses:
+        write_assistant_msg(resp)
